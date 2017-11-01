e1b1c5b8b1eef89d1139b901569f5c86690a34ec
Tjen Dekker <Tjen.Dekker@realdolmen.com>
2017-11-01 17:25:51 +0100

    initial commit

---
+++ src/main/java/com/realdolmen/model/Language.java
@@ -0,0 +1,8 @@
+package com.realdolmen.model;
+
+/**
+ * Created by TDKBG57 on 2017-10-29.
+ */
+public enum Language {
+    ENGLISH, FRENCH, SPANISH, PORTUGUESE, IIALIAN, FINISH, GERMAN, DUTCH, RUSSIAN
+}
