6aea5a082ad8e141f09fcec6e011cd3a247afad6
Tjen Dekker <Tjen.Dekker@realdolmen.com>
2017-11-02 15:34:55 +0100

    made bookstore transactional

--- src/main/java/com/realdolmen/repositories/BookRepository.java
+++ src/main/java/com/realdolmen/repositories/BookRepository.java
@@ -5,32 +5,39 @@ import com.realdolmen.model.Book;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.TypedQuery;
+import javax.transaction.Transactional;
 import java.util.List;
 
+import static javax.transaction.Transactional.TxType.REQUIRED;
+import static javax.transaction.Transactional.TxType.SUPPORTS;
+
+@Transactional(SUPPORTS)
 public class BookRepository {
     @PersistenceContext(unitName = "bookStorePU")
     private EntityManager em;
 
-    public Book find(Long id){
+    public Book find(Long id) {
         return em.find(Book.class, id);
     }
 
-    public Book create(Book book){
+    @Transactional(REQUIRED)
+    public Book create(Book book) {
         em.persist(book);
         return book;
     }
 
-    public void delete(Long id){
+    @Transactional(REQUIRED)
+    public void delete(Long id) {
         em.remove(em.getReference(Book.class, id));
     }
 
-    public List<Book> findAll(){
+    public List<Book> findAll() {
         TypedQuery<Book> query = em.createQuery("SELECT b from Book b order by b.title desc ", Book.class);
         return query.getResultList();
     }
 
 
-    public Long countAll(){
+    public Long countAll() {
         TypedQuery<Long> query = em.createQuery("select count(b) from Book b  ", Long.class);
         return query.getSingleResult();
 
